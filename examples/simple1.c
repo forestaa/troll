bd60c17fc4bc2cfd83143c30cbcc3556874eaeec
forestaa <t.daichi.3@gmail.com>
2020-01-17 18:31:21 +0900

    feat: simple contains two compilation units

---
+++ examples/simple1.c
@@ -0,0 +1,23 @@
+#include "simple2.h"
+
+typedef struct hoge {
+  int *hoge;
+  char hogehoge;
+  int array[2];
+  unsigned int fuga: 1;
+  struct student *doredore;
+} Hoge;
+
+union book {
+  char name[16];
+  int price;
+};
+
+union book bk;
+Hoge hoges[3];
+
+int main(void) {
+  sub1(c);
+  return 0;
+}
+
