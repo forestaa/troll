88ecc05a26dee15d662b20a38fcf8a4c19fa4134
forestaa <t.daichi.3@gmail.com>
2020-01-18 17:30:21 +0900

    test: extractor union type

--- tests/domain/global_variables_extractor_test.rs
+++ tests/domain/global_variables_extractor_test.rs
@@ -1,3 +1,5 @@
+extern crate troll;
+
 use troll::domain::global_variable::*;
 use troll::domain::global_variables_extractor::*;
 use troll::domain::type_entry::*;
@@ -342,6 +344,85 @@ fn extract_structure() {
             String::from("unsigned int"),
             4,
         ),
+    ];
+
+    extract_test(infos, expected_variables, expected_types);
+}
+
+#[test]
+fn extract_union() {
+    let infos = vec![
+        DwarfInfoBuilder::new()
+            .offset(Offset::new(45))
+            .tag(DwarfTag::DW_TAG_union_type)
+            .name("book")
+            .byte_size(4)
+            .children(vec![
+                DwarfInfoBuilder::new()
+                    .offset(Offset::new(58))
+                    .tag(DwarfTag::DW_TAG_unimplemented)
+                    .name("name")
+                    .type_offset(Offset::new(83))
+                    .build(),
+                DwarfInfoBuilder::new()
+                    .offset(Offset::new(70))
+                    .tag(DwarfTag::DW_TAG_unimplemented)
+                    .name("price")
+                    .type_offset(Offset::new(90))
+                    .build(),
+            ])
+            .build(),
+        DwarfInfoBuilder::new()
+            .offset(Offset::new(83))
+            .tag(DwarfTag::DW_TAG_base_type)
+            .byte_size(1)
+            .name("char")
+            .build(),
+        DwarfInfoBuilder::new()
+            .offset(Offset::new(90))
+            .tag(DwarfTag::DW_TAG_base_type)
+            .byte_size(4)
+            .name("int")
+            .build(),
+        DwarfInfoBuilder::new()
+            .offset(Offset::new(97))
+            .tag(DwarfTag::DW_TAG_variable)
+            .name("book")
+            .type_offset(Offset::new(45))
+            .location(Location::new(16428))
+            .build(),
+        DwarfInfoBuilder::new()
+            .offset(Offset::new(119))
+            .tag(DwarfTag::DW_TAG_unimplemented)
+            .name("main")
+            .type_offset(Offset::new(90))
+            .build(),
+    ];
+
+    let expected_variables = vec![GlobalVariable::new(
+        Some(Address::new(Location::new(16428))),
+        String::from("book"),
+        TypeEntryId::new(Offset::new(45)),
+    )];
+
+    let expected_types = vec![
+        TypeEntry::new_union_type_entry(
+            TypeEntryId::new(Offset::new(45)),
+            String::from("book"),
+            4,
+            vec![
+                UnionTypeMemberEntry {
+                    name: String::from("name"),
+                    type_ref: TypeEntryId::new(Offset::new(83)),
+                },
+                UnionTypeMemberEntry {
+                    name: String::from("price"),
+                    type_ref: TypeEntryId::new(Offset::new(90)),
+                },
+            ],
+        ),
+        TypeEntry::new_base_type_entry(TypeEntryId::new(Offset::new(83)), String::from("char"), 1),
+        TypeEntry::new_base_type_entry(TypeEntryId::new(Offset::new(90)), String::from("int"), 4),
     ];
 
     extract_test(infos, expected_variables, expected_types);
